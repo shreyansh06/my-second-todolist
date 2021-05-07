b26aa0e2b3df6e6eee3e3492e67b5d0657d250b8
shreyansh06 <83074405+shreyansh06@users.noreply.github.com>
2021-05-07 16:06:55 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,136 @@
+import { getLocaleDayNames } from '@angular/common';
+import { ParseTreeResult } from '@angular/compiler';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-second-project';
+  // getName(name)
+  // {
+  //   alert(name)
+  // }
+  // myEvent(evt)
+  // {
+  //   console.warn(evt)
+  // }
+  // currentval=""
+  // getValue(val){
+  //   console.warn(val)
+  //   this.currentval=val
+  // }
+  
+  // show=false
+// array 
+
+// music=['hindi','english','bhojpuri' ,'tamil']
+// car=[
+//   {
+//     Name:'honda',
+//     Price:'460000'
+
+//   },
+//   {
+//     Name:'lambo',
+//     Price:'360000'
+
+//   },
+//   {
+//     Name:'audi',
+//     Price:'26000000'
+
+//   },
+//   {
+//     Name:'thor',
+//     Price:'26000000'
+
+//   }
+
+// ]
+// getUserValue(value){
+//   console.warn(value)
+// }
+// getName(){
+//   alert("hello world")
+// }
+
+// array in carstructure
+// \
+// currentCustomer=true
+// car={
+//   name:'maruti',
+//   price: 10000
+// }
+
+
+// language=['hindi','english','tamil','urdu']
+//  1)with variable interpolation
+// name="shrey"
+// age=20
+// 2) with function interpolation
+// callMyname(){
+//   return "shreyansh kashyap"
+// }
+// with object
+// car={
+//   name:"audi",
+//   colour:"red"
+// }
+// with array
+// fruit=['apple','mango','Apricots','Avocado','Banana']
+// call function
+// clickMe(){
+//   alert("helloooooooooo")
+// }
+// getMyName(val){
+//   alert(val)
+// }
+// ng for
+// fruit=['apple','mango','Apricots','Avocado','Banana']
+// mobile=[
+//   {
+//     name:"nokia",
+//     price: 2000
+//   },
+//   {
+//     name:"redmi",
+//     price: 20000
+//   },
+//   {
+//     name:"oppo",
+//     price: 12000
+//   },
+//   {
+//     name:"vivo",
+//     price: 22000
+//   }
+// ]
+//    ng if
+// youSaySomething=true 
+// getUserValue(value){
+//  console.warn(value)
+// }
+
+// important event 1) click event
+// myEvent(evt)
+// {
+//   console.warn(evt)
+// }
+
+// property binding
+// name="bruce"
+// disabledBox=true
+// enabledBox(){
+// this.disabledBox=false
+// }
+
+// mobile=['nokia','redmi','samsung','iphone']
+
+hiiloooo=true
+
+
+
+}
